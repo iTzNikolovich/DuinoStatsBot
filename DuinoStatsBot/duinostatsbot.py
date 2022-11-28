76a92a1fe2b852f7505515f820f687cf76b71830
Niko <nicolo.carcagni@mail.com>
2022-11-28 18:20:03 +0100

    Fixed bugs

--- DuinoStatsBot/duinostatsbot.py
+++ DuinoStatsBot/duinostatsbot.py
@@ -14,76 +14,75 @@ ID = 0000 # Replace '0000' with your chat id
 WALLET = "YOUR_DUINO_USERNAME" # Replace YOUR_DUINO_USERNAME with your wallet username
 Working_dir = '' # Paste your working directory
 
-
-response = requests.get(f'https://server.duinocoin.com/users/{WALLET}')
-data = response.json()
-
-miner_names = []
-miner_hashrate = []
-miner_stats = []
-
-miner_names.clear()
-miner_names.clear()
-
-i = 0
-
-txt1 = open(f"{Working_dir}miners.txt", "w")
-txt1.close()
-
-txt2 = open(f"{Working_dir}prev_balance.txt", "r")
-prev_balance = txt2.read()
-txt2.close()
-
-duinourl = f'https://server.duinocoin.com/users/{WALLET}'
-def domoticzrequest2 (url):
-    request = urllib.request.Request(url)
-    response = urllib.request.urlopen(request)
-    return response.read()
-json_object = json.loads(domoticzrequest2(duinourl))
-
-duco_balance = json_object['result']['balance']['balance']
-txt2= open(f"{Working_dir}prev_balance.txt", "w")
-txt2.write(str(duco_balance))
-txt2.close()
-increased_balance = float(duco_balance) - float(prev_balance)
-
-minerscount = len(json_object['result']['miners'])
-
-for miners in json_object['result']['miners']:
-    miner_names.append(miners['identifier'])
-    
-for miners in json_object['result']['miners']:
-    if int(miners['hashrate']) > 1000:
-        miner_hashrate.append(str(int(miners['hashrate']/1000)) + " KH/s")
-    else:
-        miner_hashrate.append(str(int(miners['hashrate'])) + " H/s")
-
-
-for x in miner_names:
-    miner_stats.append(miner_names[i] + ": " + str(miner_hashrate[i]))
-    #print(miner_stats[i])
-    txt1 = open(f"{Working_dir}miners.txt", "a")
-    txt1.write(miner_stats[i])
-    txt1.write('\n')
-    i = i + 1
-txt1.close()
-
-txt1 = open(f"{Working_dir}miners.txt", "r")
-miners_stats = txt1.read()
-txt1.close()
-    
-
-resultstring = "________ ᕲ DuinoCoin ________"
-resultstring += '\n'
-resultstring += "💳 " + WALLET + "'s info"
-resultstring += '\n'
-resultstring += "💰 Balance: " + str(round(duco_balance,2)) + " ᕲ"  + " + "  + str(round(increased_balance,2))
-resultstring += '\n'
-resultstring += "🚜 Workers: " + str(minerscount)
-resultstring += '\n'
-resultstring += miners_stats
-
 def reply(msg):
+    response = requests.get(f'https://server.duinocoin.com/users/{WALLET}')
+    data = response.json()
+
+    miner_names = []
+    miner_hashrate = []
+    miner_stats = []
+
+    miner_names.clear()
+    miner_names.clear()
+    i = 0
+
+    txt1 = open(f"{Working_dir}miners.txt", "w")
+    txt1.close()
+
+    txt2 = open(f"{Working_dir}prev_balance.txt", "r")
+    prev_balance = txt2.read()
+    txt2.close()
+
+    duinourl = f'https://server.duinocoin.com/users/{WALLET}'
+    def domoticzrequest2 (url):
+        request = urllib.request.Request(url)
+        response = urllib.request.urlopen(request)
+        return response.read()
+    json_object = json.loads(domoticzrequest2(duinourl))
+
+    duco_balance = json_object['result']['balance']['balance']
+    txt2= open(f"{Working_dir}prev_balance.txt", "w")
+    txt2.write(str(duco_balance))
+    txt2.close()
+    increased_balance = float(duco_balance) - float(prev_balance)
+
+    minerscount = len(json_object['result']['miners'])
+
+    for miners in json_object['result']['miners']:
+        miner_names.append(miners['identifier'])
+        
+    for miners in json_object['result']['miners']:
+        if int(miners['hashrate']) > 1000:
+            miner_hashrate.append(str(int(miners['hashrate']/1000)) + " KH/s")
+        else:
+            miner_hashrate.append(str(int(miners['hashrate'])) + " H/s")
+
+
+    for x in miner_names:
+        miner_stats.append(miner_names[i] + ": " + str(miner_hashrate[i]))
+        #print(miner_stats[i])
+        txt1 = open(f"{Working_dir}miners.txt", "a")
+        txt1.write(miner_stats[i])
+        txt1.write('\n')
+        i = i + 1
+    txt1.close()
+
+    txt1 = open(f"{Working_dir}miners.txt", "r")
+    miners_stats = txt1.read()
+    txt1.close()
+        
+
+    resultstring = "________ ᕲ DuinoCoin ________"
+    resultstring += '\n'
+    resultstring += "💳 " + WALLET + "'s info"
+    resultstring += '\n'
+    resultstring += "💰 Balance: " + str(round(duco_balance,2)) + " ᕲ"  + " + "  + str(round(increased_balance,2))
+    resultstring += '\n'
+    resultstring += "🚜 Workers: " + str(minerscount)
+    resultstring += '\n'
+    resultstring += miners_stats
+
+
     chat_id = msg['chat']['id']
     command = msg['text']
     if chat_id != ID:
